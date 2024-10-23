ebd8488923d7a2bd0233d1ca1ff12a1c7cb4b735
mxrkxvka <mrmarckis@gmail.com>
2024-10-23 15:45:29 +0700

    final

--- 22102_Tolstov_3/MainWindow.xaml.cs
+++ 22102_Tolstov_3/MainWindow.xaml.cs
@@ -30,6 +30,53 @@ namespace _22102_Tolstov_3
             
         }
 
+        private void tboxArray_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(tboxArray.Text);
+
+            try
+            {
+                int start = int.Parse(tboxStart.Text);
+                int end = int.Parse(tboxEnd.Text);
+
+                if (start >= end) throw new Exception("неверно определен диапазон");
+                if (n <= 0) throw new Exception("массив не может быть размеров <= 0");
+                if (tboxStart.Text == "" || tboxStart.Text == "" || tboxEnd.Text == "") throw new Exception("поля не могут быть пустыми");
+
+                Random rnd = new Random();
+                int[] array = new int[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    array[i] = rnd.Next(start, end + 1);
+                }
+
+                array = array.OrderBy(x => x % 2 != 0).ToArray();
+
+                lblResult.Content = "Результат:\n";
+                lblResult.Content += String.Join(" ", array);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void tboxEnd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tboxStart_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
         private void tboxArraySize_TextChanged(object sender, TextChangedEventArgs e)
         {
 
